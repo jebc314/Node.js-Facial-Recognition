29687fcf1d6f9d5dd9409f3f07e999ea91e55a93
Jeb Cui <jebcui99@gmail.com>
2020-12-13 13:09:04 -0500

    Log in to spotify

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var signin_button = document.getElementById("sign-in");
+
+    signin_button.onclick = function() {
+        var callback_url = window.location.href + "spotifeeling.html/";
+        const api_url = "https://accounts.spotify.com/authorize?client_id=603a3368045e46f7ac6e93eae3b95595&response_type=token&redirect_uri="+callback_url;
+
+        window.location.replace(api_url);
+    }
+}
