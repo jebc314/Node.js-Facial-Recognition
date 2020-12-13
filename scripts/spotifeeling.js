5a3ffd090c1178ef1baff1136b38c5423995a58a
Jeb Cui <jebcui99@gmail.com>
2020-12-13 16:45:17 -0500

    still display the songs?

--- scripts/spotifeeling.js
+++ scripts/spotifeeling.js
@@ -115,6 +115,7 @@ function open_playlist() {
                 setTimeout(() => {  console.log("World!"); }, 1000);
             }).fail(function(jqXHR, textStatus) { 
                 console.error(textStatus)
+                output_paragraph.innerHTML += [track_items[i].track.name, track_items[i].track.id].toString() + "<br>";
             })
             });
         }
